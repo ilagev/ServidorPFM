64fd059488a0c5e6dae29355aff5dc3df906d0f8
Iván <ilagev@gmail.com>
2016-06-12 18:53:21 +0200

    Posting and removing reviews

--- src/main/java/business/api/ReviewResource.java
+++ src/main/java/business/api/ReviewResource.java
@@ -1,10 +1,16 @@
 package business.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import business.controllers.ReviewController;
+import business.wrappers.ReviewWrapper;
 
 @RestController
 @RequestMapping(Uris.SERVLET_MAP + Uris.REVIEWS)
@@ -17,5 +23,14 @@ public class ReviewResource {
         this.reviewController = reviewController;
     }
     
+    @RequestMapping(method = RequestMethod.POST)
+    public ReviewWrapper create(@RequestBody ReviewWrapper review, @AuthenticationPrincipal User activeUser) {
+        return reviewController.create(review, activeUser.getUsername());
+    }
+    
+    @RequestMapping(method = RequestMethod.DELETE, value = Uris.ID)
+    public void delete(@PathVariable int id) {
+        reviewController.delete(id);
+    }
 
 }
--- src/main/java/business/controllers/ReviewController.java
+++ src/main/java/business/controllers/ReviewController.java
@@ -1,12 +1,39 @@
 package business.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import business.wrappers.ReviewWrapper;
+import data.daos.ReviewDao;
+import data.daos.SmartphoneDao;
+import data.daos.UserDao;
 import data.entities.Review;
+import data.entities.Smartphone;
+import data.entities.User;
 
 @Controller
 public class ReviewController {
+    
+    private SmartphoneDao smartphoneDao;
+    
+    @Autowired
+    public void setSmartphoneDao(SmartphoneDao smartphoneDao) {
+        this.smartphoneDao = smartphoneDao;
+    }
+    
+    private UserDao userDao;
+    
+    @Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+    
+    private ReviewDao reviewDao;
+    
+    @Autowired
+    public void setReviewDao(ReviewDao reviewDao) {
+        this.reviewDao = reviewDao;
+    }
 
     public ReviewWrapper getReviewWrapper(Review r) {
         return new ReviewWrapper(
@@ -18,6 +45,18 @@ public class ReviewController {
             r.getSmartphone().getId(),
             r.getUser().getId()
         );
+    }
+
+    public ReviewWrapper create(ReviewWrapper review, String username) {
+        User user = userDao.findByNick(username);
+        Smartphone smartphone = smartphoneDao.findById(review.getSmartphoneId());
+        Review reviewEntity = new Review(review.getText(), review.getMark(), smartphone, user);
+        reviewEntity = reviewDao.save(reviewEntity);
+        return this.getReviewWrapper(reviewEntity);
+    }
+
+    public void delete(int id) {
+        reviewDao.delete(id);
     }
 
 }
